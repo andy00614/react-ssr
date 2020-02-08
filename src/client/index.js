e894b05d25ea09f83b0f4542a9ddfe42f6cc62f6
andy00614 <33732843@qq.com>
2020-02-08 11:30:02 +0800

    add webpack.client

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import Home from '../src/container/Home'
+import Home from '../container/Home'
 
 ReactDom.render(<Home />,document.getElementById('root'))
 
