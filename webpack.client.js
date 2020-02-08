3b1a93b63df9e49bd163ade29919e940e834bc28
andy00614 <33732843@qq.com>
2020-02-08 11:32:16 +0800

    配置打包client文件

---
+++ webpack.client.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/client/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    // 配置规则
+    rules: [
+      {
+        test: /\.js?$/,
+        // test: /\.(js|jsx|tsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "@babel/preset-react",
+            // "@babel/preset-stage-0", //babel7以后就不需要它了，用env来代替它
+            [
+              "@babel/env",
+              {
+                targets: {
+                  browsers: ["last 2 versions"] // 兼容主流浏览器的最后两个版本
+                },
+                useBuiltIns: "usage"
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  }
+};
